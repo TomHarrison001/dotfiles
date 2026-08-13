dba30b07c49db7f29d25aed39a738d5298d617b7
TomHarrison001 <tomharrison007@icloud.com>
2026-08-13 22:10:59 +0100

    Feat: Reduce hyprland window opacity, increase blur effect

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -83,8 +83,8 @@ hl.config({
         rounding_power = 2,
 
         -- Change transparency of focused and unfocused windows
-        active_opacity   = 1.0,
-        inactive_opacity = 1.0,
+        active_opacity   = 0.8,
+        inactive_opacity = 0.8,
 
         shadow = {
             enabled      = true,
@@ -98,6 +98,7 @@ hl.config({
             size      = 3,
             passes    = 1,
             vibrancy  = 0.1696,
+            new_optimizations = true,
         },
     },
 
